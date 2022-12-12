ad6841a1f4bb9d920fbb7038e6775d657cc30842
Raphael Haehnel <raphael.haehnel@gmail.com>
2022-12-12 20:51:14 +0200

    readme ready

--- main.py
+++ main.py
@@ -2,30 +2,49 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import functools
+import scipy.io
+import shutil
+from tqdm import tqdm
+
+LIMIT_ITER = 100
 
 
-def generate_data(n=100, c=2):
-    means = 12*(np.random.rand(c, 2) - 0.5)
+def generate_data(n, c, dim):
+    means = 12*(np.random.rand(c, dim) - 0.5)
     means = means.repeat(n, axis=0)
 
-    data = means + np.random.randn(n*c, 2)
+    data = means + np.random.randn(n*c, dim)
     col = np.zeros((n*c, 1))
     data = np.append(data, col, axis=1)
     return data
 
 
-def generate_means(k=2):
-    means = 12*(np.random.rand(k, 2) - 0.5)
+def generate_means(k, dim):
+    means = 12*(np.random.rand(k, dim) - 0.5)
     label = np.array([np.array(range(k))]).T
     return np.append(means, label, axis=1)
 
 
-def show_plot(data, mu, i, save=False):
-    plt.scatter(x=data[:, 0], y=data[:, 1], c=data[:, 2], s=20)
-    plt.scatter(x=mu[:, 0], y=mu[:, 1], c=mu[:, 2],
-                marker="*", edgecolors="black", s=100)
-    plt.xlim([-10, 10])
-    plt.ylim([-10, 10])
+def generate_means_pixels(k, dim):
+    means = np.random.rand(k, dim)
+    label = np.array([np.array(range(k))]).T
+    return np.append(means, label, axis=1)
+
+
+def show_plot(data, mu, i, save, dim):
+    if dim == 2:
+        plt.scatter(x=data[:, 0], y=data[:, 1], c=data[:, 2], s=20)
+        plt.scatter(x=mu[:, 0], y=mu[:, 1], c=mu[:, 2],
+                    marker="*", edgecolors="black", s=100)
+        plt.xlim([-10, 10])
+        plt.ylim([-10, 10])
+    if dim == 3:
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+
+        ax.scatter(data[:, 0], data[:, 1], data[:, 2], c=data[:, 3], s=20)
+        ax.scatter(mu[:, 0], mu[:, 1], mu[:, 2], c=mu[:, 3],
+                   marker="*", edgecolors="black", s=100)
 
     if save:
         if not os.path.exists('./output'):
@@ -36,82 +55,102 @@ def show_plot(data, mu, i, save=False):
         plt.show()
 
 
-def duplicate_mu(data, mu):
+def duplicate_mu(data, mu, dim):
     n_data = data.shape[0]
     n_mu = mu.shape[0]
 
     # Duplicate the values of mu along the vertical axis
     mu_v = np.apply_along_axis(functools.partial(
-        np.repeat, repeats=n_data, axis=0), axis=0, arr=mu[:, :2])
+        np.repeat, repeats=n_data, axis=0), axis=0, arr=mu[:, :dim])
 
     # Split the array into multiple sub-arrays vertically
     return np.vsplit(mu_v, n_mu)
 
 
-def assignment(data, mu):
+def assignment(data, mu, dim):
 
-    mu_duplicated = duplicate_mu(data, mu)
-    x = data[:, :2]
+    mu_duplicated = duplicate_mu(data, mu, dim)
+    x = data[:, :dim]
     result = np.zeros((1, data.shape[0]))
 
-    for i in range(mu.shape[0]):
+    for i in tqdm(range(mu.shape[0])):
         diff = x-mu_duplicated[i]
         result = np.vstack([result, np.apply_along_axis(
-            lambda a: a[0]**2+a[1]**2, 1, diff)])
+            lambda a: np.sum(a**2), 1, diff)])
     result = result[1:]
     arg_min = np.argmin(result, 0)
 
-    data[:, 2] = arg_min
+    data[:, dim] = arg_min
+    cost = np.sum(np.min(result, 0))
 
-    return data
+    return data, cost
 
 
-def helper_sum(data, k):
-    result = np.zeros((k, 2))
-    result[int(data[2])] = [data[0], data[1]]
+def helper_sum(data, k, dim):
+    result = np.zeros((k, dim))
+    result[int(data[dim])] = data[:-1]
+
     return result
 
 
-def get_linked_data(data, k):
+def cout_data_labels(data, k, dim):
 
-    sum_data_linked = np.zeros((1, k))
+    count = np.zeros((1, k))
     for row in data:
-        sum_data_linked[0, int(row[2])] += 1
+        count[0, int(row[dim])] += 1
 
-    return sum_data_linked
+    return count
 
 
-def centroid_update(data, k):
-    linked_data = get_linked_data(data, k)
-    new_mu = np.apply_along_axis(functools.partial(helper_sum, k=k), 1, data)
+def centroid_update(data, k, dim):
+    cout_labels = cout_data_labels(data, k, dim)
+    new_mu = np.apply_along_axis(
+        functools.partial(helper_sum, k=k, dim=dim), 1, data)
 
     new_mu = np.sum(new_mu, axis=0)
-    new_mu = np.divide(new_mu, linked_data.repeat(2, 0).T, out=np.zeros_like(
-        new_mu), where=linked_data.repeat(2, 0).T != 0)
+    new_mu = np.divide(new_mu, cout_labels.repeat(dim, 0).T, out=np.zeros_like(
+        new_mu), where=cout_labels.repeat(dim, 0).T != 0)
     label = np.array([np.array(range(k))]).T
     new_mu = np.append(new_mu, label, axis=1)
 
     return new_mu
 
 
-def cluster(data, mu):
-    pass
+def run_clustering(data, k, mu, dim, save):
+
+    total_cost = np.array([])
+
+    for i in range(LIMIT_ITER):
+        data, cost = assignment(data, mu, dim)
+        total_cost = np.append(total_cost, cost)
+        new_mu = centroid_update(data, k, dim)
+        if np.array_equal(new_mu, mu):
+            break
+        mu = new_mu
+        show_plot(data, mu, i, save=save, dim=dim)
+
+    return data, total_cost
+
+
+def success_rate(y_train, y_output):
+    return np.count_nonzero(y_output == y_train)/len(y_output)*100
 
 
 if __name__ == "__main__":
 
-    k = 7
-    n = 100
+    k = 5
     c = k
 
-    data = generate_data(n, c)
-    mu = generate_means(k)
-    show_plot(data, mu, 0, save=True)
+    if os.path.exists('./output'):
+        shutil.rmtree('./output')
 
-    for i in range(16):
+    n = 100
+    dim = 3
+    data = generate_data(n, c, dim)
+    mu = generate_means(k, dim)
 
-        data = assignment(data, mu)
-        show_plot(data, mu, i, save=True)
-        mu = centroid_update(data, k)
-        i += 1
-        show_plot(data, mu, i, save=True)
+    output, total_cost = run_clustering(data, k, mu, dim, save=True)
+    # result_success = success_rate(y_train, output[:, -1])
+    # print(f'Success rate = {result_success}%')
+    plt.plot(total_cost, marker="o")
+    plt.show()
